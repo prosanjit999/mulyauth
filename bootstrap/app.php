ff96c9bac7ca35827479de2a739296a3bb328ba2
prosanjit999 <bprosanjit999@gmail.com>
2024-10-09 00:11:45 -0700

    secend commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
+        $middleware->alias([
+            'guest' => RedirectIfAuthenticated::class,
+            'auth' => Authenticate::class
+        ]);
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
